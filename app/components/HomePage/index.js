d39f1c3fc23171481fec4ee78d4a91ac44cc4b98
sdromacque <sebastien@citymagine.com>
2017-12-05 14:15:47 +0100

    Replace wrapper mapbox

--- app/components/HomePage/index.js
+++ app/components/HomePage/index.js
@@ -1,83 +1,68 @@
-import React from 'react';
-import ReactMapboxGl, { ScaleControl, RotationControl, ZoomControl, Layer } from 'react-mapbox-gl';
+import React, { Props } from 'react';
+import mapboxgl from 'mapbox-gl';
+
 import 'semantic-ui-css/semantic.min.css';
 import './style.scss';
 
 import config from '../../../conf/config.json';
 
-const mapCenter = [3.057256000000052, 50.62924999999999];
-
 const optionsMapBox = {
   accessToken: config.mapbox.token,
-  style: config.mapbox.styles.default,
+  style: config.mapbox.styles,
   threed: config.mapbox.layer,
 };
 
-const Map = ReactMapboxGl({ accessToken: optionsMapBox.accessToken });
+mapboxgl.accessToken = optionsMapBox.accessToken;
 
-const HomePage = () => (
-  <div>
-    <Map
-      center={mapCenter}
-      style={optionsMapBox.style}
-    >
-      <ScaleControl
-        className="measure"
-      />
-      <ZoomControl />
-      <RotationControl
-        className="rotationControl"
-      />
-      <Layer
-        id="3d-buildings"
-        sourceId="composite"
-        sourceLayer="building"
-        filter={['==', 'extrude', 'true']}
-        type="fill-extrusion"
-        minZoom={14}
-        paint={optionsMapBox.threed}
-      />
-    </Map>
-  </div>
-);
+class HomePage extends React.Component {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      lng: 3.057256000000052,
+      lat: 50.62924999999999,
+      zoom: 13,
+    };
+  }
+
+  componentDidMount() {
+    const { lng, lat, zoom } = this.state;
+
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: optionsMapBox.style.default,
+      center: [lng, lat],
+      zoom,
+    });
+
+    map.addControl(new mapboxgl.FullscreenControl());
+
+    map.addControl(new mapboxgl.ScaleControl({
+      maxWidth: 80,
+      unit: 'metric',
+    }));
+
+    const nav = new mapboxgl.NavigationControl();
+    map.addControl(nav, 'top-right');
+
+    map.on('load', () => {
+      map.addLayer({
+        id: '3d-buildings',
+        source: 'composite',
+        'source-layer': 'building',
+        filter: ['==', 'extrude', 'true'],
+        type: 'fill-extrusion',
+        minzoom: 15,
+        paint: optionsMapBox.threed,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div id="map">
+      </div>
+    );
+  }
+}
 
 export default HomePage;
-
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import mapboxgl from 'mapbox-gl'
-//
-// mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
-//
-// class homePage extends React.Component {
-//
-//   constructor(props: Props) {
-//     super(props);
-//     this.state = {
-//       lng: 3.057256000000052,
-//       lat: 50.62924999999999,
-//       zoom: 10
-//     };
-//   }
-//
-//   componentDidMount() {
-//     const { lng, lat, zoom } = this.state;
-//
-//     const map = new mapboxgl.Map({
-//       container: this.mapContainer,
-//       style: 'mapbox://styles/mapbox/streets-v9',
-//       center: [lng, lat],
-//       zoom
-//     });
-//
-//   }
-//
-//   render() {
-//     return (
-//       <div>
-//         <div ref={el => this.mapContainer = el} className="absolute top right left bottom" />
-//       </div>
-//     );
-//   }
-// }
-// export default homePage
